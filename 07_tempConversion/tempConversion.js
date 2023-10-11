345aa5e79e4cf6583f52e19a272c16159bc2aca1
Isaiah Henson <Devpuddles@gmail.com>
2023-10-10 22:16:56 -0400

    completed all the conversion excersises

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celsius = (fahrenheit - 32) * 5 / 9
+  let roundedNun = Math.round(celsius * 10) / 10
+  return roundedNun;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fahrenheit = (celsius  * 9 / 5) + 32
+  let roundedNun = Math.round(fahrenheit * 10) / 10
+
+  return roundedNun
 };
+
+console.log(convertToFahrenheit(49.4))
+console.log(convertToCelsius(-100))
 
 // Do not edit below this line
 module.exports = {
