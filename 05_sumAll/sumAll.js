2912e816cb773bb1ab308d4dee1ae1983da1f39a
Isaiah Henson <Devpuddles@gmail.com>
2023-10-10 21:46:44 -0400

    completed all leap years excersises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,15 +1,20 @@
 const sumAll = function(num1, num2) {
     let sum = 0
-
-    for (let i = num1; i <= num2; i++){
-        sum += i
+    if (typeof num1 !== "number" || typeof num2 !== "number"){
+        return 'ERROR'
+    } else {
+        for (let i = num1; i >= num2; i--){
+        
+            sum += i
+        }
     }
+    
 
     console.log(sum)
     return sum 
 };
 
-sumAll(1, 5)
+sumAll(9, 5)
 
 // Do not edit below this line
 module.exports = sumAll;
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function(leapYear) {
+    if (leapYear % 100 === 0 && leapYear % 400 !== 0){
+        return false
+    } else if (leapYear % 400 === 0 || leapYear % 4 === 0){
+        return true
+    } else 
+        return false
 };
 
+console.log(leapYears(1997))
 // Do not edit below this line
 module.exports = leapYears;
