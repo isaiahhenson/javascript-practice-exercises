8cd2b8e315b299fa50a3001510af868cb5159279
Isaiah Henson <Devpuddles@gmail.com>
2023-10-11 21:30:43 -0400

    created dom element appended it and changed color exercise

---
+++ practice/app.js
@@ -0,0 +1,8 @@
+let container = document.querySelector('#container')
+
+let p = document.createElement('p')
+
+p.textContent = 'Hey, I"m red!'
+p.style.cssText = "color: red"
+
+container.appendChild(p)
