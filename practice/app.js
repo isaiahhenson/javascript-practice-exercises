a4a5ade531478ac0a6fa19a9957b050f7adc755c
Isaiah Henson <Devpuddles@gmail.com>
2023-10-12 16:55:50 -0400

    created dom node changed color, appended it to dom

--- practice/app.js
+++ practice/app.js
@@ -1,8 +1,13 @@
 let container = document.querySelector('#container')
 
 let p = document.createElement('p')
+let h = document.createElement('h3')
 
 p.textContent = 'Hey, I"m red!'
 p.style.cssText = "color: red"
 
+h.innerHTML = 'Hey, I"m blue!'
+h.style['color'] = 'blue'
+
 container.appendChild(p)
+container.appendChild(h)
