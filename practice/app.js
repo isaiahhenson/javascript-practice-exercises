0a3cfe415a75da000afc9a54fba2062492fc99f5
Isaiah Henson <Devpuddles@gmail.com>
2023-10-15 12:04:06 -0400

    practiced DOM manipulation, event listeners and event handling

--- practice/app.js
+++ practice/app.js
@@ -1,13 +1,28 @@
 let container = document.querySelector('#container')
+let btn = document.querySelectorAll('button')
 
 let p = document.createElement('p')
 let h = document.createElement('h3')
+let bdDiv = document.createElement('div')
+let inDiv = document.createElement('h1')
+let p2 = document.createElement('p')
+
+btn.forEach((button) => button.addEventListener('click', () => alert(button.id)))
 
 p.textContent = 'Hey, I"m red!'
 p.style.cssText = "color: red"
+bdDiv.style.border = 'solid 2px black'
+bdDiv.style.height = '100px'
+bdDiv.style['background-color'] = 'pink'
+inDiv.textContent = "I'm in a Div"
+// inDiv.style['font-size'] = '17px'
+p2.textContent = 'ME TOO!'
 
 h.innerHTML = 'Hey, I"m blue!'
 h.style['color'] = 'blue'
 
 container.appendChild(p)
 container.appendChild(h)
+container.appendChild(bdDiv)
+bdDiv.appendChild(inDiv)
+bdDiv.appendChild(p2)
