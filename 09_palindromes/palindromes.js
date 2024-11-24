e5f82181f636d52f3c7f48930e8f56b3c72b8a5e
Isaiah Henson <Devpuddles@gmail.com>
2024-11-24 18:44:16 -0500

    completed the fibonacci sequence test

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
+const palindromes = (str) => {
+  let ourStr = str.toLowerCase();
+  const reversedStr = ourStr.split('').reverse().join('');
 
+  console.log(ourStr, reversedStr);
+
+  if (ourStr === reversedStr) {
+    return true;
+  } else {
+    return false;
+  }
 };
+
+console.log(palindromes('racecar'));
 
 // Do not edit below this line
 module.exports = palindromes;
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function (int) {
+  let sequence = [0, 1];
 
+  if (int < 0) {
+    return 'OOPS';
+  } else {
+    for (let i = 2; i <= int; i++) {
+      sequence[i] = sequence[i - 1] + sequence[i - 2];
+    }
+  }
+  return sequence[sequence.length - 1];
 };
+
+console.log(fibonacci(10));
 
 // Do not edit below this line
 module.exports = fibonacci;
