e6eaae5408d209324f3f6feb47d8db595137c8a1
Isaiah Henson <Devpuddles@gmail.com>
2024-11-24 14:14:13 -0500

    completed the test for the assignment

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -7,19 +7,25 @@ const subtract = function (num1, num2) {
 };
 
 const sum = function (arr) {
-  if ((arr = [])) {
+  let total = 0;
+
+  if (arr.length === 0) {
     return 0;
   } else {
-    arr.reduce((total, current) => {
-      total + current;
-      return total;
-    }, 0);
+    for (let i = 0; i < arr.length; i++) {
+      total += arr[i];
+    }
   }
+  return total;
 };
 
-const multiply = function (num1, num2) {
-  return num1 * num2;
+const multiply = function (arr) {
+  return arr.reduce((total, current) => total * current);
 };
+
+// const multiply = function (array) {
+//   return array.reduce((product, current) => product * current);
+// };
 
 const power = function (num1, num2) {
   return Math.pow(num1, num2);
@@ -28,6 +34,9 @@ const power = function (num1, num2) {
 const factorial = function (num) {
   let total = num;
 
+  if (num === 0) {
+    total = 1;
+  }
   for (let i = 1; i < num; i++) {
     total *= i;
   }
