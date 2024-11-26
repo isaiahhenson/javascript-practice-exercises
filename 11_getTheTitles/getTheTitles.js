b6f7a70aaa3988eca51a2782ad4853b29f96bf99
Isaiah Henson <Devpuddles@gmail.com>
2024-11-25 19:57:50 -0500

    completed coding challenge

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,20 @@
-const getTheTitles = function() {
+const books = [
+  {
+    title: 'Book',
+    author: 'Name',
+  },
+  {
+    title: 'Book2',
+    author: 'Name2',
+  },
+];
 
+const getTheTitles = function (array) {
+  let newArr = array.map((obj) => obj.title);
+  return newArr;
 };
+
+getTheTitles(books);
 
 // Do not edit below this line
 module.exports = getTheTitles;
